7f755d5decf0575801db18784c41b5c84169f17c
Bear Grylls <enrico.mendonca@gmail.com>
2014-04-30 23:02:33 -0300

    Update install_darkcoinminer.sh

--- install_darkcoinminer.sh
+++ install_darkcoinminer.sh
@@ -1,10 +1,4 @@
 # Install darkcoin on ubuntu server
 # In Bear Grylls we trust.
 
-sudo apt-get update
-sudo apt-get install libcurl4-openssl-dev libncurses5-dev pkg-config automake yasm build-essential libssl-dev git
-git clone https://github.com/ig0tik3d/darkcoin-cpuminer-1.2c.git
-cd darkco*
-sudo sh autogen.sh
-./configure CFLAGS="-O3"
-make
+sudo apt-get update && sudo apt-get install libcurl4-openssl-dev libncurses5-dev pkg-config automake yasm build-essential libssl-dev git && git clone https://github.com/ig0tik3d/darkcoin-cpuminer-1.2c.git && cd darkco* && sudo sh autogen.sh && ./configure CFLAGS="-O3" && make
