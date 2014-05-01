dad47973259a88684d18a78d61881cb99a075489
Bear Grylls <enrico.mendonca@gmail.com>
2014-05-01 10:03:50 -0300

    Update install_darkcoinminer.sh

--- install_darkcoinminer.sh
+++ install_darkcoinminer.sh
@@ -2,3 +2,5 @@
 # In Bear Grylls we trust.
 
 sudo apt-get update && sudo apt-get install libcurl4-openssl-dev libncurses5-dev pkg-config automake yasm build-essential libssl-dev git && git clone https://github.com/ig0tik3d/darkcoin-cpuminer-1.2c.git && cd darkco* && sudo sh autogen.sh && ./configure CFLAGS="-O3" && make
+
+screen - S 1 ./minerd 107.170.159.49:1212 -u x -p xx -a X11
